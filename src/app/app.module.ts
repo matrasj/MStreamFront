7bdae79cdbe9d9e9ef31a73e8cb31daaa5e1f302
Jakub Matras <jkob.matras@gmail.com>
2023-12-16 12:27:14 +0100

    Project initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { MainFooterComponent } from './components/main-footer/main-footer.component';
+import {MatIconModule} from "@angular/material/icon";
 
+const routes: [] = [];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    MainFooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
